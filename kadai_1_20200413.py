f4f7cbdbd74e69b0b4b6e52c567d4aba05c4628c
naozin555 <naozin555@gmail.com>
2020-04-14 00:44:35 +0900

    データの前処理途中まで

--- kadai_1_20200413.py
+++ kadai_1_20200413.py
@@ -1,2 +1,18 @@
-with open('area-2019.csv', 'r') as f:
-    print(f.read())
+
+count = 0
+
+with open('area-2019_a.csv', 'w+') as fa:
+    with open('area-2019.csv', 'r') as f:
+        for line in f:
+            count += 1
+            if count == 2:
+                l2 = line.strip()
+                # print(l2)
+            if count == 3:
+                # ,を削除して結合
+                l3 = l2 + line.strip()[2:]
+                # print(l3)
+                fa.write(l3)
+            if count == 4:
+                fa.write(line)
+                break
