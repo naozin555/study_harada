dd9b7a32addcb05414d636d20d46942a64832184
naozin555 <naozin555@gmail.com>
2020-04-15 03:57:56 +0900

    ファイルへの出力を試みた

--- kadai_1_20200413.py
+++ kadai_1_20200413.py
@@ -1,4 +1,6 @@
 import re
+import csv
+import numpy as np
 
 with open('area-2019.csv', 'r') as f:
 
@@ -16,8 +18,16 @@ with open('area-2019.csv', 'r') as f:
         if ':' in line:
             data_list.append(line.strip())
 
-print(header)
-for data in data_list:
-    print(data)
+# print(header)
+# for data in data_list:
+#     print(data)
+
+def convert_1d_to_2d(l, cols):
+    return [l[i:i + cols] for i in range(0, len(l), cols)]
 
 
+with open('area-2019_a.csv', 'w') as fa:
+    fa.write(header + '\n')
+    convert_1d_to_2d(data_list, 2)
+    writer = csv.writer(fa, lineterminator='\n', delimiter=',')
+    writer.writerows(data_list)
