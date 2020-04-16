6d2488ef3c6d5b41195e77ee4cea82de7bf44575
naozin555 <naozin555@gmail.com>
2020-04-17 02:39:02 +0900

    2019年4月の表示完了。これを毎月分コード書くとえぐいので，こちらも別途演算クラスを定義して，月毎の割合算出メソッドを作ろうと思う。

--- kadai_1_20200413.py
+++ kadai_1_20200413.py
@@ -20,11 +20,13 @@ with open('area-2019.csv', 'r') as f:
 headers = header.split(',')
 print(headers)
 
+for data in data_list:
+    print(data.split(','))
 
 class Data(object):
     def __init__(self, date, time, demand, nuclear, thermal, water, geo_thermal,
                  biomass, solar, controlled_solar, wind, controlled_wind,
-                 pumped_water, interconnection, total):
+                 pumped_water, interconnection, total_supply):
         self.date = date
         self.time = time
         self.demand = demand
@@ -39,14 +41,76 @@ class Data(object):
         self.controlled_wind = controlled_wind
         self.pumped_water = pumped_water
         self.interconnection = interconnection
-        self.total = total
+        self.total_supply = total_supply
 
-# for i
-#     data = Data(data_list[0][i], data_list[1][i], data_list[2][i], data_list[3][i],
-#                 data_list[4][i], data_list[5][i], data_list[6][i], data_list[8][i],
-#                 data_list[9][i], data_list[10][i], data_list[11][i], data_list[12][i],
-#                 data_list[13][i], data_list[14][i], data_list[15][i])
-#
-# print(data_list[1])
+datas_list = []
+for data in data_list:
+    datas = Data(data.split(',')[0], data.split(',')[1], data.split(',')[2],
+                 data.split(',')[3], data.split(',')[4], data.split(',')[5],
+                 data.split(',')[6], data.split(',')[7], data.split(',')[8],
+                 data.split(',')[9], data.split(',')[10], data.split(',')[11],
+                 data.split(',')[12], data.split(',')[13], data.split(',')[14],
+                 )
+    datas_list.append(datas)
 
+# 2019年4月1日の火力の出力
+print(datas_list[0].thermal)
+
+# ４月　変数定義
+total_demand_2019_4 = 0
+total_nuclear_2019_4 = 0
+total_thermal_2019_4 = 0
+total_water_2019_4 = 0
+total_geo_thermal_2019_4 = 0
+total_biomass_2019_4 = 0
+total_solar_2019_4 = 0
+total_controlled_solar_2019_4 = 0
+total_wind_2019_4 = 0
+total_controlled_wind_2019_4 = 0
+total_pumped_water_2019_4 = 0
+total_interconnection_2019_4 = 0
+total_total_supply_2019_4 = 0
+
+# 4月　需給合計
+for data_every_hour in datas_list:
+    if data_every_hour.date.startswith('2019/4'):
+        total_demand_2019_4 += int(data_every_hour.demand)
+        total_nuclear_2019_4 += int(data_every_hour.nuclear)
+        total_thermal_2019_4 += int(data_every_hour.thermal)
+        total_water_2019_4 += int(data_every_hour.water)
+        total_geo_thermal_2019_4 += int(data_every_hour.geo_thermal)
+        total_biomass_2019_4 += int(data_every_hour.biomass)
+        total_solar_2019_4 += int(data_every_hour.solar)
+        total_controlled_solar_2019_4 += int(data_every_hour.controlled_solar)
+        total_wind_2019_4 += int(data_every_hour.wind)
+        total_controlled_wind_2019_4 += int(data_every_hour.controlled_wind)
+        total_pumped_water_2019_4 += int(data_every_hour.pumped_water)
+        total_interconnection_2019_4 += int(data_every_hour.interconnection)
+        total_total_supply_2019_4 += int(data_every_hour.total_supply)
+
+# 4月　需給割合　算出
+percentage_nuclear_2019_4 = total_nuclear_2019_4/total_total_supply_2019_4*100
+percentage_thermal_2019_4 = total_thermal_2019_4/total_total_supply_2019_4*100
+percentage_water_2019_4 = total_water_2019_4 / total_total_supply_2019_4*100
+percentage_geo_thermal_2019_4 = total_geo_thermal_2019_4 / total_total_supply_2019_4*100
+percentage_biomass_2019_4 = total_biomass_2019_4 / total_total_supply_2019_4*100
+percentage_solar_2019_4 = total_solar_2019_4 / total_total_supply_2019_4*100
+percentage_controlled_solar_2019_4 = total_controlled_solar_2019_4 / total_total_supply_2019_4 * 100
+percentage_wind_2019_4 = total_wind_2019_4 / total_total_supply_2019_4*100
+percentage_controlled_wind_2019_4 = total_controlled_wind_2019_4 / total_total_supply_2019_4*100
+percentage_pumped_water_2019_4 = total_pumped_water_2019_4 / total_total_supply_2019_4*100
+percentage_interconnection_2019_4 = total_interconnection_2019_4 / total_total_supply_2019_4*100
+
+# 4月　需給割合　出力
+print('2019/4 原子力：{:.2f}%'.format(percentage_nuclear_2019_4))
+print('2019/4 火力：{:.2f}%'.format(percentage_thermal_2019_4))
+print('2019/4 水力：{:.2f}%'.format(percentage_water_2019_4))
+print('2019/4 地熱：{:.2f}%'.format(percentage_geo_thermal_2019_4))
+print('2019/4 バイオマス：{:.2f}%'.format(percentage_biomass_2019_4))
+print('2019/4 太陽光発電実績：{:.2f}%'.format(percentage_solar_2019_4))
+print('2019/4 太陽光出力制御量：{:.2f}%'.format(percentage_controlled_solar_2019_4))
+print('2019/4 風力発電実績：{:.2f}%'.format(percentage_wind_2019_4))
+print('2019/4 風力出力制御量：{:.2f}%'.format(percentage_controlled_wind_2019_4))
+print('2019/4 揚水：{:.2f}%'.format(percentage_pumped_water_2019_4))
+print('2019/4 連系線：{:.2f}%'.format(percentage_interconnection_2019_4))
 
